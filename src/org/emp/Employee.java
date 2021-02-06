685a9dcf81419f78560d29b4be7b59fe9d05659b
VijayK1395 <vjvija073@gmail.com>
2021-02-06 18:23:41 +0530

    3rd update

--- src/org/emp/Employee.java
+++ src/org/emp/Employee.java
@@ -21,7 +21,11 @@ public class Employee {
 
 	}
 	private void empAdd() {
-		// TODO Auto-generated method stub
+		System.out.println("emp");
+
+	}
+	private void empCity() {
+		System.out.println("emp");
 
 	}
 	
